c0fb581017fe4eade9f75b323814f00888ad2146
Laman <Laman@DESKTOP-B13SN36>
2020-10-05 23:34:56 -0700

    Version 2

--- cse360a2/src/cse360assignment02/AddingMachine.java
+++ cse360a2/src/cse360assignment02/AddingMachine.java
@@ -8,36 +8,44 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
-	
+	StringBuffer str1;
+
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		str1 = new StringBuffer("0");
 	}
 	  
 	/**
 	 * return the total
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	/**
 	* add 
 	*/	  
 	public void add (int value) {
+		total += value;
+		str1.append(" + " + value);
 	}
 	/**
 	* subtract
 	*/	
 	public void subtract (int value) {
+		total -= value;
+		str1.append(" - " + value);
 	}
 	/**
 	* toString to display the operations made
 	*/
 	public String toString () {
-		return "";
+		return str1.toString();
 	}
 	/**
 	* clear the total
 	*/
 	public void clear() {
+		total = 0;
 	}
 }
+
