9df1e1ac3e6442ed4136e1a703922a6368b7ec2b
Laman <Laman@DESKTOP-B13SN36>
2020-10-05 23:15:51 -0700

    CSE360 Assignment 2

---
+++ cse360a2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,43 @@
+/**
+* Lamanh Ngo
+* 10/5/2020
+* CSE360
+* Assignment 2
+*/
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	  
+	/**
+	 * return the total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	* add 
+	*/	  
+	public void add (int value) {
+	}
+	/**
+	* subtract
+	*/	
+	public void subtract (int value) {
+	}
+	/**
+	* toString to display the operations made
+	*/
+	public String toString () {
+		return "";
+	}
+	/**
+	* clear the total
+	*/
+	public void clear() {
+	}
+}
